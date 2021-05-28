55c832406419ee61a226d3e02346af5d201bc8b4
SebastienCajiao <sebastiencajiao@gmail.com>
2021-05-27 18:57:08 -0600

    Intercambio de colores
    El personaje cuando toca el objeto llave cambian de color

--- Assets/Scripts/ZonaReinicio.cs
+++ Assets/Scripts/ZonaReinicio.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
+
 public class ZonaReinicio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Miembros de clase pública.
+    public Transform destino;
 
-    // Update is called once per frame
-    void Update()
+    // Método que se ejecuta cuando este objeto interseca otro objeto en el juego.
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Consultamos si el objeto que interseca tiene la etiqueta del jugador.
+        if(other.CompareTag("Player"))
+        {
+            // Se reubica el avatar del jugador en otra ubicación de la escena.
+            other.transform.position = destino.position;
+
+            PlayerData playerdata = other.GetComponent<PlayerData>();
+            // con playerdata referenciado al jugador podemos hacer cosas como quitarle una vida al personaje.
+            // ejemplo: 
+            // playerdata.vidas--;
+        }
     }
 }
