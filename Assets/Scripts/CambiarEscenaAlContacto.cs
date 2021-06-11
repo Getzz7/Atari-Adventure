bf821911cdcbe46813bc9008fd10207e0114c05d
SebastienCajiao <sebastiencajiao@gmail.com>
2021-06-10 19:34:39 -0600

    Sonidos,Prefabs, Codigos
    Agregamos sonido a las puertas, cambiamos el codigo, e hicimos un Prefabs

--- Assets/Scripts/CambiarEscenaAlContacto.cs
+++ Assets/Scripts/CambiarEscenaAlContacto.cs
@@ -8,11 +8,14 @@ public class CambiarEscenaAlContacto : MonoBehaviour
     public string SiguienteEscena;
     public Transform PosicionRespawn;
     public Player ObjetoJugador;
+    public TypeOfSound sonido = TypeOfSound.Door;
     
     private void OnTriggerEnter(Collider other)
  {
     if (other.CompareTag("Player"))
  {
+    // Aqui va el codigo para guardar el sonido
+    Sound_Handler.Instance.playsound((int)sonido);
     //Aqui va el codigo para guardar la puerta de Destino
     GameManager.instance.SetDoor(puertaDestino.ToString());
     GameManager.instance.ChangeScenes(SiguienteEscena);
---
+++ Assets/Scripts/TypeOfSound.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeOfSound 
+{
+    Door = 0,
+    Key = 1,
+    EnemyHit = 2,
+}
