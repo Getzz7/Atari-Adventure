15e2a98c14a387d542a35bbc67634f258a824043
SebastienCajiao <sebastiencajiao@gmail.com>
2021-07-08 15:29:19 -0600

    Escenas Menu
    Agregue las escenas de los menu con el cambio de las escenas

---
+++ Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambioEscena(string nombreEscena)
+    {
+        Debug.Log("Hola, has hecho click en este boton");
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+    }
+}
