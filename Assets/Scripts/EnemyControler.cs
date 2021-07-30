9e1c8be32eaa5252285730a663224431aea9d9e4
SebastienCajiao <sebastiencajiao@gmail.com>
2021-07-29 19:17:50 -0600

    Implementacion del enemigo
    creamos dos codigos para el enemigo, uno del movimiento y el otro el collider

---
+++ Assets/Scripts/EnemyControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyControler : MonoBehaviour
+{
+    public float distanciaMinima;
+    private float distanciaActual; 
+    public Transform Jugador; 
+    public Transform PosicionInicial;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // el enemigo persigue al jugador
+       distanciaActual = Vector3.Distance(Jugador.position, this.transform.position);
+       Debug.Log(distanciaActual); 
+
+       if(distanciaActual < distanciaMinima)
+       {
+           // La distancia actual es menor a la minima persigue al jugador
+            agent.SetDestination(Jugador.position);
+       }
+       else
+       {
+           // No perseguir al jugar e ir a la posicion inicial
+           agent.SetDestination(PosicionInicial.position);
+       }
+    }
+}
---
+++ Assets/Scripts/PlayerTouch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTouch : MonoBehaviour
+{
+   void OnTriggerEnter(Collider other)
+   {
+       if(other.gameObject.tag == "Player")
+       {
+           // toque al jugador y el jugador perdio el juego
+           
+       }
+   }
+}
